6333babaff71c6cf7cf581a1529655b3ccc5f1a4
me <kelvelord80@gmail.com>
2022-10-18 12:10:57 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,10 @@
 
 /**
  * print_alphabet_x10 - prints all alphabets in lowercase 10 times
+ * Return: On success 1.
  */
 void print_alphabet_x10(void)
 {
-	int 10;
 	char la;
 
 	for (ten = 0; ten <= 9; ten++)
