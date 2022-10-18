82da8656e59e89f1e2c040557b2d895f3dfcfd1d
me <kelvelord80@gmail.com>
2022-10-18 11:31:57 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabeth - prints the alpha in a lowercase
+ */
+void print_alphabeth(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
