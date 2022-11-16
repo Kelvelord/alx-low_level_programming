9362be682bd87d1ca1c3d81aae6c7b8edeb25840
me <kelvelord80@gmail.com>
2022-11-15 23:55:54 -0800

    task 333

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,28 +1,68 @@
-#include "3-calc.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * get_op_func - a function ...
- * @s: the chaine
- * Return: 1 or 0
+ * * op_add- a function ...
+ * * @a: the tab of integer
+ * * @b: the number
+ * * Return: 1 or 0
  */
-int (*get_op_func(char *s))(int a, int b)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (i < 5)
+/**
+ * * op_sub- a function ...
+ * * @a: the tab of integer
+ * * @b: the number
+ * * Return: 1 or 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * * op_mul- a function ...
+ * * @a: the tab of integer
+ * * @b: the number
+ * * Return: 1 or 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * * op_div- a function ...
+ * * @a: the tab of integer
+ * * @b: the number
+ * * Return: 1 or 0
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-	if (*ops[i].op == *s)
-	return (ops[i].f);
-	i++;
+	printf("Error\n");
+	exit(100);
 	}
-	return (NULL);
+	return (a / b);
+}
+
+/**
+ * * op_mod- a function ...
+ * * @a: the tab of integer
+ * * @b: the number
+ * * Return: 1 or 0
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	return (a % b);
 }
