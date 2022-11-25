42d604e283b3a138020ffb74365abecca0b90372
me <kelvelord80@gmail.com>
2022-11-24 18:46:58 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,133 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: unsigned int.
+ * Return: the int converted from the string
  */
-unsigned int binary_to_uint(const char *b)
+int _atoi(char *s)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i, d, n, len, f, digit;
 
-	if (!b)
-		return (0);
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	ui = 0;
+	while (s[len] != '\0')
+		len++;
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	while (i < len && f == 0)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
+		if (s[i] == '-')
+			++d;
 
-		if (b[len] & 1)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			ui += base_two;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
+		i++;
 	}
 
-	return (ui);
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * rev_string - a function that reverse a string
+ * @s: chaine of caractere
+ *
+ * Return: 1 or 0
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, taille, k;
+	char c;
+
+
+	while (s[i] != '\0')
+		i++;
+
+	i--;
+	taille = i;
+	k = i / 2;
+	i = 0;
+
+	while (i <= k)
+	{
+		c = s[i];
+		s[i] = s[taille];
+		s[taille] = c;
+		i++;
+		taille--;
+	}
+}
+
+/**
+ * puis - the function calcule the x puissance y
+ * @x: the number
+ * @y: the number
+ *
+ * Return: 1 or 0
+ */
+
+int puis(int x, int y)
+{
+	int n = 1, i;
+
+	for (i = 0; i < y; i++)
+		n = n * x;
+
+	return (n);
+}
+
+/**
+ * binary_to_uint - a function ....
+ * @b: the chaine of caractere
+ *
+ * Return: 1 or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int n = 0, i = 0, cpt = 0, test;
+	char c;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+		i++;
+	i--;
+
+	while (i >= 0)
+	{
+		test = (b[i] == '0' || b[i] == '1');
+		if (!test)
+		{
+			/*printf("test=%d pour i=%d\n",test,i);*/
+			return (0);
+		}
+		c = b[i];
+		n = n + _atoi(&c) * puis(2, cpt);
+		/*printf("b[%d]=%d\n",i,_atoi(&c));*/
+		i--;
+		cpt++;
+	}
+	return (n);
 }
