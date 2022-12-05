8cf3f5669a0cb00f406ba4859afe2ceb3076c8e3
me <kelvelord80@gmail.com>
2022-12-05 10:11:16 -0800

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,40 @@
+#include <stdio.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 9096
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - a function ...
+ * @filename: the list
+ * @letters: the number
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: 1 or 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fp, ret;
+	char buf[BUF_SIZE + 1];
+	size_t cpt = 1;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fp = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (fp == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
+	while ((ret = read(fp, buf, letters)))
+	{
+		buf[ret] = '\0';
+		printf("%s", buf);
+		cpt = cpt + ret;
+	}
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	close(fp);
+	return (cpt - 1);
 }
